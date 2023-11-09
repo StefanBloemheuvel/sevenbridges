1f104c8b9c1fda91250d0c6517dec50ed165381d
StefanBloemheuvel <stefanbloemheuvel@gmail.com>
2023-11-09 14:28:41 +0100

    changed name of packages

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup
 
 setup(
     name='sevenbridges',
-    version='0.0.7',
+    version='0.0.8',
     description='',
     long_description='',
     author='',
     author_email='',
     url='https://pypi.org/project/sevenbridges',
     license='',
-    # packages=['sevenbridges'],
-    py_modules=['sevenbridges'],
+    packages=['src'],
     python_requires='>=3.8',
     install_requires=[
         'pandas',
